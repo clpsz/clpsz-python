41b60a3b6230b0f70ba777a3527130746890b1c7
clpsz <pengclpsz@gmail.com>
2021-11-25 14:24:07 +0800

    use python3

--- clpsz/clpsz_flag.py
+++ clpsz/clpsz_flag.py
@@ -7,7 +7,7 @@ def main(number):
 	binary_str = "{0:b}".format(number)
 	binary_str = binary_str[::-1]
 	for i, c in enumerate(binary_str):
-		print '{:02d}: {:s}'.format(i, c)
+		print('{:02d}: {:s}'.format(i, c))
 
 
 if __name__ == '__main__':
